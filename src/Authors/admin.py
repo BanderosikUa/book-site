8722b2ebecc39edc8d7e3a5ce9ecfa531b2438a2
BanderosikUa <darnnest31@gmail.com>
2022-05-18 19:26:14 +0300

    Admin for book, genre and authir

--- src/Authors/admin.py
+++ src/Authors/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Author
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ("name", "slug", "biography")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class AuthorAdminInline(admin.StackedInline):
+    model = Author
+
+
+admin.site.register(Author, AuthorAdmin)
 
 # Register your models here.
--- src/Books/admin.py
+++ src/Books/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from Authors.models import Author
+from .models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'genre', 'count_views', 'time_modified')
+    search_fields = ('name', 'author')
+    readonly_fields = ('time_modified', 'time_created')
+    fields = ('name', 'slug', 'about', 'photo', 'author', 'genre',
+              'time_created', 'time_modified')
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Book, BookAdmin)
 
 # Register your models here.
