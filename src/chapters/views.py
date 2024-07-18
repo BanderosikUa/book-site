addd00bca9f2fc0a0c70f8839c64905de435e7bb
BanderosikUa <darnnest31@gmail.com>
2024-07-18 18:44:37 +0300

    add tabulation

--- src/chapters/views.py
+++ src/chapters/views.py
@@ -21,10 +21,12 @@ class ChaptersListView(ListView):
 
         return context
 
+
 def add_notification_to_navbar_view(request):
     user = request.user
     response = add_notification_to_navbar(user)
     return JsonResponse(response)
+
 
 def delete_notification(request, notification_pk):
     user = request.user
