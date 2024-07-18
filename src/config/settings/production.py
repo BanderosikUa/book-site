5b19102f71109fb0e52590c8b5effe05d79d30a4
BanderosikUa <darnnest31@gmail.com>
2024-07-18 18:45:04 +0300

    add production files

---
+++ src/config/settings/production.py
@@ -0,0 +1,21 @@
+import os
+
+from .base import *
+
+from dotenv import load_dotenv
+load_dotenv()
+
+DEBUG = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": os.getenv("POSTGRES_DB", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        'HOST': 'db',
+        'PORT': '5432',
+        }
+    }
