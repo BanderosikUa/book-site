57662a2e1113e4c8a0d248d4cf4f8c9650ffb29d
BanderosikUa <darnnest31@gmail.com>
2024-07-18 19:09:25 +0300

    .

--- src/authors/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from os import environ
-from django import setup
-environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-setup()
--- src/authors/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .views import *
-
-urlpatterns = [
-    path('author/all/', AuthorAllView.as_view(), name='all-authors'),
-    path('author/<slug:author_slug>/', AuthorView.as_view(), name='author'),
-]
