fcb091c75ba584f8ddd5211641a753d84bc6443b
BanderosikUa <darnnest31@gmail.com>
2024-09-11 15:07:53 +0300

    add api exception handler

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -217,18 +217,12 @@ MANAGERS = ADMINS
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer'
-    ),
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework.parsers.JSONParser',
-    ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'EXCEPTION_HANDLER': 'api.handlers.hacksoft_proposed_exception_handler',
 }
