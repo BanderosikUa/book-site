ca9bac135e456c57503eda4b05386557b2d83ec5
BanderosikUa <darnnest31@gmail.com>
2024-09-21 16:32:28 +0300

    remove unused code

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import *
-from .views.comment_viewsets import CommentBookViewSet
-
 app_name = 'apiv1'
 
 router = routers.SimpleRouter()
-router.register(r'book', BookViewSet)
-router.register(r'comment', CommentBookViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls')),
-    # path('comment/<int:book_pk>/', CommentBookViewSet.as_view({'get': 'list'}))
 ]
--- src/api/views/__init__.py
+++
@@ -1,1 +0,0 @@
-from .book_viewsets import *
